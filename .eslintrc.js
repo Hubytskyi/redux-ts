0c40232bdf3d0353cd8981b399868e4f8f4584e9
Mykola Hubytskyi <mykolahubytskyi@Mykolas-MacBook-Pro.local>
2022-11-20 14:58:15 +0200

    change react/jsx-filename-extension rule eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     allowComputed: 'off',
     indent: 'off',
     'react/no-danger': 'off',
@@ -20,6 +20,6 @@ module.exports = {
     'prettier/prettier': 'off',
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-    "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
+    'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
   },
 };

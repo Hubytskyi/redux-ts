3ed202e91bdd61a9dc60eee8f21d9f4ab9bfd569
Mykola Hubytskyi <mykolahubytskyi@Mykolas-MacBook-Pro.local>
2022-11-20 22:17:07 +0200

    change readme

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import { Box, Typography } from '@mui/material';
 const Hero = () => {
   return (
     <Box sx={{ mt: 20 }}>
-      <Typography variant={'h1'}>TypeScript, RTK Query, MUI, React testing library!</Typography>
+      <Typography variant={'h1'}>TypeScript, RTK Query, MUI, React testing library</Typography>
     </Box>
   );
 };
